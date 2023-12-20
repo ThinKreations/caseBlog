30072032a27e0cf7bf3af8acaa0b0d44cfc8ea3d
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2023-12-20 16:22:19 -0600

    bobesponja

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,24 @@ import styles from "../styles/Home.module.css"
 export default function Home() {
   return (
     <>
-    <MainHead titulo="Hola"/>
-    <Header/>
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <font size="4">
-        <p><b>En el vertiginoso mundo de la tecnología, la eficiencia y la precisión son pilares fundamentales para cualquier proyecto. En el ámbito del desarrollo de software, dos tipos de herramientas han transformado radicalmente la manera en que abordamos la creación y gestión de programas: las herramientas automatizadas y las herramientas CASE. <br/><br/> En esta era de innovación constante, la automatización se ha convertido en una aliada indispensable, agilizando procesos, reduciendo errores y liberando tiempo valioso para la creatividad y el enfoque estratégico. Por otro lado, las herramientas CASE han revolucionado la forma en que diseñamos, desarrollamos y mantenemos software, proporcionando un arsenal de recursos que potencian la productividad y la calidad en cada etapa del ciclo de vida del desarrollo. <br/><br/> En este blog, nos sumergiremos en el mundo de estas herramientas poderosas y versátiles. Exploraremos cómo impactan en la eficiencia de los procesos, cómo optimizan el flujo de trabajo y cómo pueden llevar tus proyectos de software a un nivel superior.</b></p>
-        </font>
+      <MainHead titulo="Auto n Case Blog"/>
+      <Header/>
+      <div className={styles.container}>
+        <center>
+        <div className={styles.content}>
+          <font size="4">
+          <p><b>En el vertiginoso mundo de la tecnología, la eficiencia y la precisión son pilares fundamentales para cualquier proyecto. En el ámbito del desarrollo de software, dos tipos de herramientas han transformado radicalmente la manera en que abordamos la creación y gestión de programas: las herramientas automatizadas y las herramientas CASE. <br/><br/> En esta era de innovación constante, la automatización se ha convertido en una aliada indispensable, agilizando procesos, reduciendo errores y liberando tiempo valioso para la creatividad y el enfoque estratégico. Por otro lado, las herramientas CASE han revolucionado la forma en que diseñamos, desarrollamos y mantenemos software, proporcionando un arsenal de recursos que potencian la productividad y la calidad en cada etapa del ciclo de vida del desarrollo. <br/><br/> En este blog, nos sumergiremos en el mundo de estas herramientas poderosas y versátiles. Exploraremos cómo impactan en la eficiencia de los procesos, cómo optimizan el flujo de trabajo y cómo pueden llevar tus proyectos de software a un nivel superior.</b></p>
+          </font>
+        </div>
+        </center>
+        <div className={styles.container_1}>
+            <center><h1 className={styles.titulos}>Herramientas Automáticas</h1></center>
+            <div className={styles.container_2}>
+              
+            </div>
+        </div>
       </div>
-    </div>
-    <Footy/>
+      <Footy/>
     </>
   )
 }
