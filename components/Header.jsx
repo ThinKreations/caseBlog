6009b3d9d2a4e6f84303d1afd483e95c6d0c719d
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2023-12-21 02:49:37 -0600

    Pongan AURORA

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import logo from '../src/logo.png'
+import { useRouter } from 'next/router';
 export default function Header(){
-    
+    const router = useRouter();
 
 
     return(
@@ -14,10 +15,10 @@ export default function Header(){
                 <Image className={styles.image} src={logo} width={250} height={52} alt="Logo"/>
             </center>
             <div>
-                <a href="#inicio"><button type="button" className={styles.btn}>Incio</button></a>
+                <a href="#inicio"><button type="button" className={styles.btn}>Inicio</button></a>
                 <a href="#herramientas-automatizadas"  ><button type="button" className={styles.btn}>Herramientas automatizdas</button></a>
                 <a href="#herramientas-case"><button type="button" className={styles.btn}>Herramientas CASE</button></a>
-                <a href="https://cass.best"><button type="button" className={styles.btn}>Enlaces de interez</button></a>
+                <a href="https://example.com/"><button type="button" className={styles.btn}>Enlaces de interez</button></a>
             </div>
         </div>
         </>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import MainHead from '../components/MainHead'
 import Footy from '../components/Footy'
 import Header from '../components/Header'
-import Header1 from '../components/Header2'
 import Image from 'next/image'
 import styles from "../styles/Home.module.css"
 import herramientas1 from '../src/automatizadas1.png'
 import diagrama1 from '../src/diagrama_1.png'
 import gantt1 from '../src/gantt_1.png'
+import auto from '../src/auto.png'
 export default function Home() {
   return (
+    
       <>
       <MainHead titulo="Auto n Case Blog"/>
       <Header/>
@@ -20,6 +21,7 @@ export default function Home() {
           </font>
         </div>
         </center>
+        <center>
         <div className={styles.container_1} id="herramientas-automatizadas">
             <center><h1 className={styles.titulos}>Herramientas Automátizadas</h1></center>
             <center>
@@ -33,7 +35,7 @@ export default function Home() {
               </div>
             </center>
         </div>
-        <div className={styles.container_1} id="herramientas-case">
+        <div className={styles.container_Y} id="herramientas-case">
             <center><h1 className={styles.titulos} id="case">Herramientas CASE</h1></center>
             <center>
               <div className={styles.container_2}>
@@ -47,13 +49,12 @@ export default function Home() {
               </div>
               
             </center>
-        </div>
-        <div className={styles.container_1}>
+            <br/><br/>
             <center><h1 className={styles.titulos} id="finalidad">Finalidad de las herramientas CASE</h1></center>
             <center>
               <div className={styles.container_2}>
               <div className={styles.container_3}>
-              <font size="4" face="Quicksand" color="white">
+              <font size="4" face="Quicksand">
                 <p>La finalidad de estas herramientas es que sean empleadas en el desarrollo de software. Ayudando a reducir el coste en el desarrollo del software, asimismo aumentar la calidad final del software o del sistema en desarrollo. </p>
                 <p>Este tipo de herramientas estan diseñadas para facilitar la gestión de proyectos de desarrollo de software automatizando tareas de desarrollo. Intentando garantizar una alta calidad en el software detectando errores para que estos sean corregidos. Aparte de que ofrece un lugar centralizado para gestionar el desarrollo de software, lo que tambien ayuda a incrementar la productividad de los desarrolladores.</p>
                 <br></br>
@@ -67,14 +68,53 @@ export default function Home() {
                   <li>Hace mas facil del desarrollo del software.</li>
                 </ul>
                 <br/><br/>
-                <p>Estas herramientas han demostrado mejorar la calidad y productividad de los desarrolladores, además de facilitar el mantenimiento del software. Durante el desarrollo y ciclo de vida de este.</p>
+                <p>Estas herramientas han demostrado mejorar la calidad y productividad de los desarrolladores, además de facilitar el mantenimiento del software durante el desarrollo y ciclo de vida de este.</p>
+                <br/>
+                <p>Estas se dividen en: <b>Upper, Lower e Integrated</b></p>
+
               </font>
               </div>
                 <Image src={gantt1} className={styles.image_1}/>
               </div>
-              
             </center>
         </div>
+        <div className={styles.container_Y3}>
+            <center><h1 className={styles.titulos} id="finalidad">Herramientas L-CASE (Lower CASE)</h1></center>
+            <center>
+              <div className={styles.container_2}>
+                <div className={styles.container_3}>
+                  <font size="4" face="Quicksand">
+                    <p>Este tipo de herramientas CASE se dedican principalmente a: </p>
+                    <br></br>
+                    <ul>
+                      <li>Semi-automatizar la generación de código.</li>
+                      <li>Crear programas que detecten errores. </li>
+                      <li>Depurar programas o pruebas. </li>
+                      <li>Automatizar la documentación de la aplicación. </li>
+                    </ul>
+                    <br/><br/>
+                    <p>Una herramienta considerada de Lower CASE es “Worksoft” ya que permite varias de las funciones anteriores, como prueba de los anterior, la herramienta ayuda a la automatización de los desarrollos “Agil-DevOps” para aplicaciones empresariales complejas mediante el procesamiento de datos se SAP (Sistemas, Aplicaciones y Productos).</p>
+                    <br/><br/>
+                    <p>La detección de errores que realiza es gracias a que tiene la capacidad de probar procesos comerciales complejos para entornos de aplicaciones combinadas al mismo tiempo que como se mencionó anteriormente, al tener procesamiento de SAP, las pruebas incluyen el probar dicho procesamiento. También soporta pruebas “Agile-plus-DevOps" y con ayuda del procesamiento SAP se vuelven más certeras.</p>
+                    <br/><br/>
+                    <p>Por parte de la semi-automatización de código, a pesar de generar una solución sin código debido a su enfoque orientado a la experiencia del negocio y del cliente y su objetivo de lograr la excelencia en los procesos empresariales, cuenta con el soporte web y en nube con optimizaciones preconfiguradas para poder utilizarlas a lo largo del código y llegar a un resultado más acertado a la solución propuesta, además estas configuraciones son de uso común por lo que son básicamente compatibles con cualquier tipo de código para cualquier tipo de aplicación.</p>
+                    <br/><br/>
+                    <p>Para depurar el programa es necesario mencionar que cuenta con reconocimiento de objetos para SAP Fiori y se mantiene continuamente actualizado por lo que aquellos datos requeridos para la parte de procesamiento de SAP son asignados automáticamente y se va ajustando de acuerdo a las necesidades de la aplicación y de las actualizaciones.</p>
+                    <br/><br/>
+                    <p>Automatiza la documentación de la aplicación ya que descubre y genera la documentación automáticamente y autónomamente debido a la información otorgada tanto por el usuario y el procesamiento SAP. Así como es capaz de generar la documentación básica, debido a su compatibilidad con los sistemas ALM (Aplication Lifecycle Managment) y las cadenas de herramientas “DevOps” puede generar la documentación necesaria para ambas como el ciclo de vida que tendrá el sistema.</p>
+                  </font>
+                </div>
+                <p><a href="https://www.worksoft.com/resources/watch-video-worksoft-connective-automation-platform-2021?wvideo=1sl6tqgbiq" className={styles.link_clear} target='_blank'><Image src={auto} width={300} height={300} className={styles.imageCircle}/><br/><center>Haz clic en la imagen</center></a></p><br/>
+                
+                {/* 
+                <video autoPlay loop style={{ width: '500px', height: '500px' }}>
+                  <source src="https://www.worksoft.com/resources/watch-video-worksoft-connective-automation-platform-2021?wvideo=1sl6tqgbiq" />
+                </video>
+                */}
+              </div>
+            </center>
+        </div>
+        </center>
       </div>
       <Footy/>
     </>
